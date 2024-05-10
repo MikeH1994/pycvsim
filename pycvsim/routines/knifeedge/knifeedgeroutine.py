ae42d1094252c3c5a617e4bc86677fd9227d04a5
Michael Hayes <michael.hayes@npl.co.uk>
2024-05-10 20:38:57 +0100

    changed number of samples in knife edge routine

--- pycvsim/routines/knifeedge/knifeedgeroutine.py
+++ pycvsim/routines/knifeedge/knifeedgeroutine.py
@@ -30,10 +30,10 @@ class KnifeEdgeRoutine:
         p1 = edge_image_points[1]
         # compute a mask that correlates to the line
         mask = np.zeros((self.camera.yres, self.camera.xres), dtype=np.uint8)
-        cv2.line(mask, (int(p0[0]), int(p0[1])), (int(p1[0]), int(p1[1])), 1, thickness=3)
+        cv2.line(mask, (int(p0[0]), int(p0[1])), (int(p1[0]), int(p1[1])), 1, thickness=6)
 
         image = self.renderer.render_image(camera_index=0, n_samples=1, return_as_8_bit=False)
-        image[mask > 0] = self.renderer.render_image(camera_index=0, n_samples=500**2, mask=mask, return_as_8_bit=False)[mask > 0]
+        image[mask > 0] = self.renderer.render_image(camera_index=0, n_samples=50**2, mask=mask, return_as_8_bit=False)[mask > 0]
 
         edge = Edge(image, p0, p1)
         esf_x, esf_f = edge.get_edge_profile(normalise=False, search_region=4)
