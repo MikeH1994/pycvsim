c8bf9d5614f353bdc40adba4d1589b97db2e7ecc
Michael Hayes <michael.hayes@npl.co.uk>
2024-05-12 22:41:43 +0100

    Added siemens star target

---
+++ example_scripts/testing/targets/siemens_star.py
@@ -0,0 +1,21 @@
+import numpy as np
+from pycvsim.sceneobjects.targets.siemensstar import SiemensStar
+from pycvsim.rendering.panda3drenderer import Panda3DRenderer
+from pycvsim.rendering.open3drenderer import Open3DRenderer
+from pycvsim.rendering.scenecamera import SceneCamera
+from pycvsim.core.image_utils import overlay_points_on_image
+import matplotlib.pyplot as plt
+
+res = (720, 640)
+mesh = SiemensStar(radius=0.5, n_spokes=72)
+cameras = [SceneCamera(pos=np.array([0.0, 0.0, -3]), res=res, hfov=30.0)]
+renderer = Open3DRenderer(cameras=cameras, objects=[mesh])
+
+while True:
+    for theta in np.linspace(-180, 180, 100):
+        mesh.set_euler_angles(np.array([0, 0, theta]))
+        image_points = renderer.cameras[0].get_pixel_point_lies_in(mesh.get_object_points())
+        img_1 = renderer.render(0, n_samples=10**2)
+        # img_1 = overlay_points_on_image(img_1, image_points, radius=6)
+        plt.imshow(img_1)
+        plt.show()
--- pycvsim/sceneobjects/targets/utils.py
+++ pycvsim/sceneobjects/targets/utils.py
@@ -1,5 +1,6 @@
 import open3d as o3d
 import numpy as np
+from typing import List
 from numpy.typing import NDArray
 from pycvsim.sceneobjects.utils import tensor_mesh_to_legacy_mesh
 
@@ -25,32 +26,35 @@ def create_cylinder(position, radius, depth, color):
 
     return cylinder
 
+def create_sector_of_circle(theta_0: float, theta_1: float, radius: float, n_segments: int = 5, centre=np.zeros(3),
+                            colour: NDArray = np.array([255.0, 255.0, 255.0])):
+    def create_segment(index_0_, index_1_, triangle_indices_: List = None):
+        #if (index_1_ - index_0_) % 2 == 1:
+        #    raise Exception("foo")
+        if triangle_indices_ is None:
+            triangle_indices_ = []
+        diff = (index_1_ - index_0_) // 2
+        index_mid = index_0_ + diff
+        triangle_indices_ += [[index_0_, index_1_, index_mid]]
+        if diff <=2:
+            return triangle_indices_
+        triangle_indices_ = create_segment(index_0_, index_mid, triangle_indices_)
+        triangle_indices_ = create_segment(index_mid, index_1_, triangle_indices_)
+        return triangle_indices_
 
-def create_box_with_hole(bottom_left, hole_pos, square_width, square_height,
-                         depth, circle_radius, square_color):
-    square = create_box(bottom_left, square_width, square_height, depth, square_color)
-    dx = 0.0 * depth
-    hole_pos = [hole_pos[0], hole_pos[1], hole_pos[2] - dx/2]
-    cutout_1 = create_cylinder(hole_pos, circle_radius, depth + dx, [0.0, 0.0, 0.0])
-    cutout_2 = o3d.geometry.TriangleMesh.create_sphere(circle_radius).translate([hole_pos[0], hole_pos[1], hole_pos[2]])
-    cutout_3 = o3d.geometry.TriangleMesh.create_sphere(circle_radius).translate([hole_pos[0], hole_pos[1],
-                                                                                hole_pos[2] + depth])
-    # o3d.visualization.draw_geometries([square, cutout_1])  # square
+    n_points = (2**n_segments + 1)
+    vertices = np.full((n_points, 3), fill_value=centre)
+    theta = np.linspace(theta_0, theta_1, vertices.shape[0]-1)
+    vertices[1:, 0] += radius*np.cos(theta)
+    vertices[1:, 1] += radius*np.sin(theta)
+    vertex_colours = np.full(vertices.shape, fill_value=colour, dtype=np.float32) / 255.0
+    triangle_indices = np.array(create_segment(1, n_points-1, [[0, 1, n_points-1]]), dtype=np.int32)
 
-    # convert to tensor tensor_mesh so we can do boolean stuff
-    # o3d.visualization.draw_geometries([square])  # square
-    square = o3d.t.geometry.TriangleMesh.from_legacy(square)
-    cutout_1 = o3d.t.geometry.TriangleMesh.from_legacy(cutout_1)
-    cutout_2 = o3d.t.geometry.TriangleMesh.from_legacy(cutout_2)
-    cutout_3 = o3d.t.geometry.TriangleMesh.from_legacy(cutout_3)
-
-    #square = square.boolean_difference(cutout_1) # , tolerance=1.0
-    #square = square.boolean_difference(cutout_2) # , tolerance=1.0
-    square = square.boolean_difference(cutout_3) # , tolerance=1.0
-
-    square = tensor_mesh_to_legacy_mesh(square)
-    o3d.visualization.draw_geometries([square])  # square
-    return square
+    mesh = o3d.geometry.TriangleMesh()
+    mesh.vertices = o3d.utility.Vector3dVector(vertices)
+    mesh.vertex_colors = o3d.utility.Vector3dVector(vertex_colours)
+    mesh.triangles = o3d.utility.Vector3iVector(triangle_indices)
+    return mesh
 
 
 def transform_object_points(object_points: NDArray, translation: NDArray, euler_angles: NDArray, degrees=True):
